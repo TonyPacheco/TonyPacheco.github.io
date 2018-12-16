fe4111df19299e83e0007777c6f59ff221085daf
TonyPacheco <tonypacheco.cillis@gmail.com>
2018-12-15 22:04:31 -0800

    added clear and exit

--- console.js
+++ console.js
@@ -3,14 +3,15 @@ var lines = document.getElementById("output");
 
 function intro() {
     lines.innerHTML = "Welcome to Tony Pacheco\'s personal website!";
-    writeToConsole("You can type 'help' for a list of commands.");
-    writeToConsole("(c) Tony Pacheco 2018");
+    writeToConsole(0, "You can type 'help' for a list of commands");
+    writeToConsole(0, "or 'exit' if you don't love terminals like Tony");
+    writeToConsole(0, "(c) Tony Pacheco 2018");
 }
 
 input.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
         var userInput = getUserInput();
-        writeToConsole(userInput);
+        writeToConsole(1, userInput);
         validateCommand(userInput);
     }
 });
@@ -40,45 +41,59 @@ function validateCommand(text) {
         return;
     } 
     if(checkText === "linkedin") {
-        writeToConsole("opening in a new tab");
+        writeToConsole(0, "opening in a new tab");
         window.open("https://www.linkedin.com/in/tony-pacheco");
         return;
     } 
     if(checkText === "github") {
-        writeToConsole("opening in a new tab");
+        writeToConsole(0, "opening in a new tab");
         window.open("https://github.com/TonyPacheco");
         return;
     } 
+    if(checkText === "clear") {
+        lines.innerHTML = ">clear";
+        return;
+    }
+    if(checkText === "exit") {
+        writeToConsole(0, "redirecting to boring version of the site");
+        window.location.href = "";
+        return;
+    }
     commandNotFound(text);
 }
 
-function writeToConsole(text) {
+function writeToConsole(isInput, text) {
     var currentText = lines.innerHTML;
+    if(isInput){
+        text = ">" + text
+    }
     lines.innerHTML = currentText + "\n" + text;
     lines.scrollTop = lines.scrollHeight;
 }
 
 function commandNotFound(text) {
-    writeToConsole("command \'" + text + "\' not found");
+    writeToConsole(0, "command \'" + text + "\' not found");
 }
 
 function showListOfCommands() {
-    writeToConsole("");
-    writeToConsole("RESUME     See Tony's resume");
-    writeToConsole("SNSAPP     Seek N Solve Android app");
-    writeToConsole("MEALIO     Food management web app");
-    writeToConsole("LINKEDIN   Visit Tony's LinkedIn profile");
-    writeToConsole("GITHUB     Visit Tony's GitHub profile");
+    writeToConsole(0, "");
+    writeToConsole(0, "RESUME     See Tony's resume");
+    writeToConsole(0, "SNSAPP     Seek N Solve Android app");
+    writeToConsole(0, "MEALIO     Food management web app");
+    writeToConsole(0, "LINKEDIN   Visit Tony's LinkedIn profile");
+    writeToConsole(0, "GITHUB     Visit Tony's GitHub profile");
+    writeToConsole(0, "CLEAR      Clears the screen")
+    writeToConsole(0, "EXIT       Visit Tony's 'regular' site");
 }
 
 function showAsciiResume() {
-    writeToConsole("WIP");
+    writeToConsole(0, "WIP");
 }
 
 function showSeekNSolve() {
-    writeToConsole("WIP");
+    writeToConsole(0, "WIP");
 }
 
 function showMealio() {
-    writeToConsole("WIP");
+    writeToConsole(0, "WIP");
 }
