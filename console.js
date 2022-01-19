318be8c5b550b49ed3b059c23240dfd89ea212c5
Tony Pacheco <tony@virtro.ca>
2022-01-18 23:11:25 -0800

    Code cleanup, added pdf resume

--- console.js
+++ console.js
@@ -1,65 +1,36 @@
-var input = document.getElementById("input");
-var lines = document.getElementById("output");
-var aux   = document.getElementById("aux");
+const input = document.getElementById("input");
+const lines = document.getElementById("output");
+const aux   = document.getElementById("aux");
 
-function intro() {
-    lines.innerHTML = "Welcome to Tony Pacheco\'s personal website!";
-    writeToConsole(0, "You can type 'help' for a list of commands");
-    writeToConsole(0, "or 'exit' if you don't love terminals like Tony");
-    writeToConsole(0, "(c) Tony Pacheco " + new Date().getFullYear());
+document.body.onload = function(){
     input.focus();
 }
 
+terminal.addEventListener("click", function (event) {
+    input.focus();
+});
+
 input.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
-        var userInput = getUserInput();
+        let userInput = getUserInput();
         writeToConsole(1, userInput);
         validateCommand(userInput);
     }
 });
 
-terminal.addEventListener("click", function(event){
-    input.focus();
-});
-
 function getUserInput() {
-    var text = input.value;
+    let text = input.value;
     input.value = ""; 
     return text;
 }
 
 function validateCommand(text) {
-    checkText = text.toLowerCase();
-    if(checkText === "help") {
-        showListOfCommands();
-    }
-    else if(checkText === "resume") {
-        toggleResume();
-    }
-    else if(checkText === "hema-app") {
-        redirect("https://github.com/TonyPacheco/NoDoubles");
-    }
-    else if(checkText === "mealio") {
-        redirect("http://mealio.tk");
-    } 
-    else if(checkText === "linkedin") {
-        redirect("https://www.linkedin.com/in/tony-pacheco");
-    } 
-    else if(checkText === "github") {
-        redirect("https://github.com/TonyPacheco");
-    } 
-    else if(checkText === "swords") {
-        redirect("https://www.instagram.com/_tonypacheco/");
-    }
-    else if(checkText === "clear") {
-        lines.innerHTML = ">clear";
-    }
-    else if(checkText === "exit") {
-        exitConsole();
-    }
-    else{
+    checkText = text.toUpperCase();
+    let command = commands[checkText];
+    if (command)
+        command.exec();
+    else
         commandNotFound(text);
-    }
 }
 
 function redirect(url){
@@ -68,7 +39,7 @@ function redirect(url){
 }
 
 function writeToConsole(isInput, text) {
-    var currentText = lines.innerHTML;
+    let currentText = lines.innerHTML;
     if(isInput){
         text = ">" + text
     }
@@ -82,27 +53,68 @@ function commandNotFound(text) {
 
 function showListOfCommands() {
     writeToConsole(0, "");
-    if (aux.style.display == 'none'){
-        writeToConsole(0, "RESUME    See Tony's resume");
+    for(const key in commands){
+        commands[key].help();
     }
-    else{
-        writeToConsole(0, "RESUME    Hide Tony's resume");
-    }
-    writeToConsole(0, "HEMA-APP  Tournament Assistant Android app");
-    writeToConsole(0, "MEALIO    Food management web app");
-    writeToConsole(0, "LINKEDIN  Visit Tony's LinkedIn profile");
-    writeToConsole(0, "GITHUB    Visit Tony's GitHub profile");
-    writeToConsole(0, "SWORDS    See Tony's other hobby");
-    writeToConsole(0, "CLEAR     Clears the screen");
-    writeToConsole(0, "EXIT      Visit Tony's 'regular' site");
 }
 
 function toggleResume() {
-    aux.style.display = (aux.style.display == 'none') ? 'block' : 'none';
+    aux.style.display = resumeVisible() ? 'none' : 'block';
+}
+
+function resumeVisible() {
+    return aux.style.display == 'block';
+}
+
+function clearConsole() {
+    lines.innerHTML = ">clear"
 }
 
 function exitConsole() {
     /*writeToConsole(0, "redirecting to the boring version of the site");*/
     /*window.location.href = "";*/
     writeToConsole(0, "lol jk");
+}
+
+const commands = {
+    "HELP": {
+        help: () => { },
+        exec: () => showListOfCommands()
+    },
+    "RESUME" : {
+        help: () => writeToConsole(0, `RESUME    ${resumeVisible() ? "Hide" : "See"} my résumé`),
+        exec: () => toggleResume()
+    },
+    "HEMA-APP": {
+        help: () => writeToConsole(0, "HEMA-APP  Tournament Assistant Android app"),
+        exec: () => redirect("https://github.com/TonyPacheco/NoDoubles")
+    },
+    "STAR-LOG": {
+        help: () => writeToConsole(0, "STAR-LOG  Astro-Photo session tracking app"),
+        exec: () => redirect("http://starlog.azurewebsites.net")
+    },
+    "LINKEDIN": {
+        help: () => writeToConsole(0, "LINKEDIN  Visit my LinkedIn profile"),
+        exec: () => redirect("https://www.linkedin.com/in/tony-pacheco")
+    },
+    "GITHUB": {
+        help: () => writeToConsole(0, "GITHUB    Visit my GitHub profile"),
+        exec: () => redirect("https://github.com/TonyPacheco")
+    },
+    "SWORDS": {
+        help: () => writeToConsole(0, "SWORDS    See my other hobby"),
+        exec: () => redirect("https://www.instagram.com/_tonypacheco/")
+    },
+    "CONTACT": {
+        help: () => writeToConsole(0, "CONTACT   Send me electronic mail"),
+        exec: () => redirect("mailto:tonypacheco.cillis@gmail.com")
+    },
+    "CLEAR": {
+        help: () => writeToConsole(0, "CLEAR     Clears the screen"),
+        exec: () => clearConsole()
+    },
+    "EXIT": {
+        help: () => writeToConsole(0, "EXIT      Visit my 'regular' site"),
+        exec: () => exitConsole()
+    }
 }
